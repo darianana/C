594704c2b0ad248f3cf5f1a256014c384009bd9f
Dariana <dariana.rr@mail.ru>
2019-12-20 23:04:34 +1000

    cats

---
+++ CATS 3B/cmake-build-debug/CMakeFiles/CATS_3B.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CATS_3B.dir/main.cpp.obj"
+  "CATS_3B.pdb"
+  "CATS_3B.exe"
+  "CATS_3B.exe.manifest"
+  "libCATS_3B.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CATS_3B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
